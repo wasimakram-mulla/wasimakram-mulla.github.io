18e781d50b2076f13e434d7d1a583157a2fa9ea5
Wasimakram <wasim3ace@gmail.com>
2016-03-23 09:57:29 +0530

    Update app.js
    Calculation of age done

--- js/Controllers/app.js
+++ js/Controllers/app.js
@@ -1,29 +1,33 @@
-var portfolio = angular.module('portfolio', [
-'ngRoute',
-'ngResource'
-]);
-
-portfolio.config(['$routeProvider','$resourceProvider',
-  function($routeProvider,$resourceProvider) {
-    $routeProvider.
-      when('/profile', {
-        templateUrl: 'views/profile.html'
-      }).
-      when('/experience', {
-        templateUrl: 'views/experience.html'
-      }).
-      when('/abilities', {
-        templateUrl: 'views/abilities.html'
-      }).
-      when('/projects', {
-        templateUrl: 'views/projects.html'
-      }).
-      when('/contact', {
-        templateUrl: 'views/contact.html'
-      }).
-      otherwise({
-        redirectTo: '/profile'
-      });
-	  
-}]);
-  
+var portfolio = angular.module('portfolio', [
+'ngRoute',
+'ngResource'
+]);
+
+portfolio.config(['$routeProvider','$resourceProvider',
+  function($routeProvider,$resourceProvider) {
+    $routeProvider.
+      when('/profile', {
+        templateUrl: 'views/profile.html'
+      }).
+      when('/experience', {
+        templateUrl: 'views/experience.html'
+      }).
+      when('/abilities', {
+        templateUrl: 'views/abilities.html'
+      }).
+      when('/projects', {
+        templateUrl: 'views/projects.html'
+      }).
+      when('/contact', {
+        templateUrl: 'views/contact.html'
+      }).
+      otherwise({
+        redirectTo: '/profile'
+      });
+}]);
+
+portfolio.controller("HomeController", function($scope){
+	var sdt = new Date('1972-11-30');
+	var difdt = new Date(new Date() - sdt);
+	$scope.age=(difdt.toISOString().slice(0, 4) - 1970) + "Years " + (difdt.getMonth()+1) + "Months " + difdt.getDate() + "Days";
+});
